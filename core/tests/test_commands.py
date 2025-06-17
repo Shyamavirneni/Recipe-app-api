350b61c72b2c150cc52d3c8c65f87039e400a4ba
ShyamAvirneni <shyamavirneni@gmail.com>
2025-06-17 12:29:45 +0530

    Fix remaining flake8 linting errors

--- core/tests/test_commands.py
+++ core/tests/test_commands.py
@@ -10,15 +10,17 @@ from django.db.utils import OperationalError
 from django.test import SimpleTestCase
 
 @patch("core.management.commands.wait_for_db.Command.check")
+
+
 class CommandTests(SimpleTestCase):
     """Test commands."""
     def test_wait_for_db_ready(self, patched_check):
         """Test waithing for database ifdatabase ready."""
         patched_check.retuern_value = True
-        
         call_command("wait_for_db")
         patched_check.assert_called_once_with(databases=["default"])
     @patch('time.sleep')
+    
     def test_wait_for_db_delay(self, patched_sleep, patched_check):
         """Test waiting for database when getting operationalerror."""
         patched_check.side_effect = [Psycopg2Error] * 2 + \
