5e2e6502697e41182f172ff0b8e94bcf6e32487b
ShyamAvirneni <shyamavirneni@gmail.com>
2025-06-18 10:44:53 +0530

    pie

--- core/management/commands/wait_for_db.py
+++ core/management/commands/wait_for_db.py
@@ -17,10 +17,13 @@ class Command(BaseCommand):
         db_up = False
         while db_up is False:
             try:
+                # Check if the default database is available
                 self.check(databases=["default"])
                 db_up = True
             except (Psycopg2Error, OperationalError):
+                # If not available, print message and wait
                 self.stdout.write("Database unavailable, waiting 1 second...")
                 time.sleep(1)
+        # Once database is available, print success message
         self.stdout.write(self.style.SUCCESS("Database available!"))
 
